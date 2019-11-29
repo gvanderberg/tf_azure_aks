307edd2cf73c34ea7354e7eccbef59fe0fac22ac
Gareth van der Berg <gareth.shaun@gmail.com>
2019-11-29 09:57:02 +0200

    update vnet

--- src/modules/vnet/main.tf
+++ src/modules/vnet/main.tf
@@ -2,13 +2,13 @@ resource "azurerm_virtual_network" "this" {
   name                = var.network_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/8"]
+  address_space       = ["10.51.12.0/22"]
   tags                = var.tags
 }
 
 resource "azurerm_subnet" "this" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
-  address_prefix       = "10.240.0.0/16"
+  address_prefix       = "10.51.12.0/23"
   virtual_network_name = azurerm_virtual_network.this.name
 }
