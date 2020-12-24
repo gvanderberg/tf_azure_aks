08d9a87de6c4f4d478428f743815de55e950853a
Gareth van der Berg <gareth.shaun@gmail.com>
2020-12-24 13:58:45 +0200

    formatting update

--- src/providers.tf
+++ src/providers.tf
@@ -8,8 +8,6 @@ provider "helm" {
   kubernetes {
     load_config_file       = "false"
     host                   = module.aks.host
-    # username               = module.aks.cluster_username
-    # password               = module.aks.cluster_password
     client_certificate     = base64decode(module.aks.client_certificate)
     client_key             = base64decode(module.aks.client_key)
     cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
@@ -19,8 +17,6 @@ provider "helm" {
 provider "kubernetes" {
   load_config_file       = "false"
   host                   = module.aks.host
-  # username               = module.aks.cluster_username
-  # password               = module.aks.cluster_password
   client_certificate     = base64decode(module.aks.client_certificate)
   client_key             = base64decode(module.aks.client_key)
   cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
